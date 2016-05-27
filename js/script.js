7aa12ea8ec10357c591ecddba89b3071b136bdee
David Conner <agilebluefox@gmail.com>
2016-05-27 08:53:08 -0400

    Added js function to submit the add item button and append the value of the input box to the shopping list.

--- js/script.js
+++ js/script.js
@@ -5,21 +5,36 @@ $(document).ready(function() {
     // Use the click function to add and remove the appropriate class
     // when the user clicks on the checkbox icon
     checkbox.click(function() {
-        if ($(this).hasClass("fa-square-o")) {
-            $(this).removeClass("fa-square-o").addClass("fa-check-square-o");
-        } else if ($(this).hasClass("fa-check-square-o")) {
-            $(this).removeClass("fa-check-square-o").addClass("fa-square-o");
-        }
+        if ($(this).hasClass( "fa-square-o")) {
+            $(this).removeClass( "fa-square-o" ).addClass( "fa-check-square-o" );
+        } else if ($(this).hasClass( "fa-check-square-o" )) {
+            $(this).removeClass( "fa-check-square-o" ).addClass( "fa-square-o" );
+        };
         // Finally, add or remove the "done" class to the list item
-        $(this).parent( "li" ).toggleClass("done");
+        $(this).parent( "li" ).toggleClass( "done" );
     });
 
     // Set a variable to contain the target for the delete action
-    var trash = $( "i.remove");
+    var trash = $( "i.remove" );
 
     // Use the remove function to delete the list item selected
     trash.click(function() {
-        $(this).parent("li").remove();
+        $(this).parent( "li" ).remove();
+    });
+
+    // Use the append function to add an item to the list
+    var add = $( "#add-item" );
+
+    add.submit( function( event ) {
+        console.log( $("input").val() );
+        if ( $("input").val() !== '' ) {
+            $("ul.list").append('<li class="list-item">\
+                <i class="fa fa-square-o checkbox" aria-hidden="true"></i><span>'
+                + $("input").val()
+                + '</span><i class="fa fa-trash remove" aria-hidden="true"></i></li>');
+            console.log("You're adding a list item");
+            event.preventDefault();  //didn't work without this line (see jquery api)
+        };
     });
 
 });
