db590088b7764be532fe57d88256140a1985eb02
David Conner <agilebluefox@gmail.com>
2016-05-27 14:08:15 -0400

    Cleaned up jQuery functions to target only the icon images and added an effect to fade the list item when being deleted from the list.

--- js/script.js
+++ js/script.js
@@ -2,32 +2,30 @@ $(document).ready(function() {
 
     // Use the click action to add and remove the appropriate class
     // when the user clicks on the checkbox icon
-    $('ul.list').on('click', 'li', function() {
-        $(this).toggleClass('done');
+    $('ul.list').on('click', '.checkbox', function() {
+        $(this).parent('li').toggleClass('done');
 
-        if ($(this).children('i.checkbox').hasClass( "fa-square-o" )) {
-            $(this).children('i.checkbox').removeClass( "fa-square-o" );
-            $(this).children('i.checkbox').addClass( "fa-check-square-o" );
+        if ($(this).hasClass( "fa-square-o" )) {
+            $(this).removeClass( "fa-square-o" );
+            $(this).addClass( "fa-check-square-o" );
         } else {
-            $(this).children('i.checkbox').removeClass( "fa-check-square-o" );
-            $(this).children('i.checkbox').addClass( "fa-square-o" );
+            $(this).removeClass( "fa-check-square-o" );
+            $(this).addClass( "fa-square-o" );
         }
     });
 
-    // Use the remove function to delete the list item selected
+    // Use the fadeOut function to delete the list item selected
+    // with a neat animation effect
     $('ul.list').on('click', '.remove', function() {
-        $(this).parent('li').remove();
+        $(this).parent('li').fadeOut(1000);
     });
 
     // Use the append function to add an item to the list
+    var liPrefix = '<li class="list-item"><i class="fa fa-square-o checkbox" aria-hidden="true"></i><span>';
+    var liSuffix = '</span><i class="fa fa-trash remove" aria-hidden="true"></i></li>';
     $( "#add-item" ).submit( function( event ) {
-        console.log( $("input").val() );
         if ( $("input").val() !== '' ) {
-            $("ul.list").append('<li class="list-item">\
-                <i class="fa fa-square-o checkbox" aria-hidden="true"></i><span>'
-                + $("input").val()
-                + '</span><i class="fa fa-trash remove" aria-hidden="true"></i></li>');
-            console.log("You're adding a list item");
+            $("ul.list").append(liPrefix + $("input").val() + liSuffix);
             //didn't work without this line (see jquery api)
             // It seems like this is necessary to prevent the browser's
             // default action which is to submit the form
